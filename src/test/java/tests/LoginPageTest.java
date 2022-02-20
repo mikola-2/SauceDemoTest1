8ae5989db9a1c5873bcf5601fd5d70d4e9cfd025
Nikolay Kudryashov <kudryashov.net@gmail.com>
2022-02-20 15:09:42 +0300

    Check IP.

--- src/test/java/tests/LoginPageTest.java
+++ src/test/java/tests/LoginPageTest.java
@@ -4,6 +4,7 @@ import io.qameta.allure.Issue;
 import io.qameta.allure.Link;
 import io.qameta.allure.TmsLink;
 import lombok.extern.log4j.Log4j2;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
@@ -16,11 +17,13 @@ public class LoginPageTest extends BaseTest {
     @TmsLink("")
     @Test (description = "Input correct username and password.")
     public void inputValidData() {
-        log.info("Input correct username and password.");
-        loginPage.open();
-        loginPage.login("standard_user", "secret_sauce");
-        assertTrue(inventoryPage.isProductsDisplayed(), "Couldn't log in to the site");
+//        log.info("Input correct username and password.");
+//        loginPage.open();
+//        loginPage.login("standard_user", "secret_sauce");
+//        assertTrue(inventoryPage.isProductsDisplayed(), "Couldn't log in to the site");
 
+        driver.get("https://www.google.com/search?q=my+ip&oq=my+ip&aqs=chrome.0.69i59j0i512l6j69i60.1775j0j7&sourceid=chrome&ie=UTF-8");
+        Assert.assertTrue(false);
     }
 
     @Test (description = "Input correct username and empty password.")
